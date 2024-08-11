ec6d3d00521f0b2f85afd550c09bd8008100eb2f
EngineerNayabBano <nayab.bano2000@gmail.com>
2024-08-11 22:50:28 +0500

    operators
    operators in typescript

---
+++ operators/main.js
@@ -0,0 +1,46 @@
+"use strict";
+// console.log("hello world");
+// arthematic mean
+// addition operator
+// console.log(10+10);
+// let num1 =10;
+// let num2 =20;
+// let num3 ='s';
+// let sum =num1 +num2;
+// console.log(sum);
+// substraction
+// let defference= num1 -num2;
+// console.log(defference);
+//multipication
+// let product =num1*num2;
+// console.log(product);
+//division
+// let quotient= num1/num2;
+// console.log(quotient);
+//modulus(%)/ it provide reminder of two number
+// let reminder=num1%num2;
+// console.log(reminder);
+// let num1=20;
+// let num2=10;
+// let reminder=num1%num2;
+// console.log(reminder+1);
+// console.log(10%3);
+// relational/ comparision operator(==,===,<,<=,>,>=)
+// let num1=20;
+// let num2=10;
+// let num3='s';
+// let num4=20;
+// let num5=5;
+// equals to ==,===
+// console.log(num1==num2);
+// console.log(num1===num2);
+// less than or greater than
+// console.log(num1<num2);
+// console.log(num1>num2);
+//less than equal to <=
+//  console.log(num1>=num2);
+//greater than equal to
+// console.log(num1>=num2);
+//example question
+let fruit = 'peach';
+console.log(fruit == peach);
